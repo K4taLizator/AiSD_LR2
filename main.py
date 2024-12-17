2890330b9a0130074d22d62b563d63a5df89f4b8
K4taLizator <120108487+K4taLizator@users.noreply.github.com>
2024-12-17 23:48:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from RB import RedBlackTree
 from BT import BinaryTree
 import graphics
 import random
-#size = [ i*1000 for i in list(range(1, 20))]
-size = [20]
+import sys
+sys.setrecursionlimit(100000)
+size = [ i*1000 for i in list(range(1, 20))]
+#size = [20]
 keys = list(range(1, max(size)*2))
-random.shuffle(keys)
+#random.shuffle(keys)
 
 avl_h = []
 rb_h = []
@@ -24,12 +26,15 @@ for i in size:
     avl_h.append(graphics.find_height(avl_tree.root))
     rb_h.append(graphics.find_height(rb_tree.root))
     bt_h.append(graphics.find_height(bt_tree.root))
-"""
+"""graphics.visualize(avl_tree.root, "avl") 
+avl_tree.delete(9) 
+graphics.visualize(avl_tree.root, " New tree")"""
+
 graphics.plots(size, avl_h, "AVL") 
 graphics.plots(size, rb_h, "Red Black") 
 graphics.plots(size, bt_h, "Binary") 
-"""
-graphics.visualize(rb_tree.root, "rb_tree")
+
+"""graphics.visualize(rb_tree.root, "rb_tree")
 
 print("Postorder traversal")
 graphics.postorder_traversal(rb_tree.root)
@@ -38,7 +43,7 @@ graphics.inorder_traversal(rb_tree.root)
 print("\nPreorder traversal")
 graphics.preorder_traversal(rb_tree.root)
 print("\nWide traversal")
-graphics.wide_traversal(rb_tree.root)
+graphics.wide_traversal(rb_tree.root)"""
 """
 graphics.visualize(avl_tree.root, "avl_tree")
 graphics.visualize(rb_tree.root, "rb_tree")"""
